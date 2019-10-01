a0087ed50a1d3d8590e9618e13b8c2fee53460ce
Veto1234-ii <vetoshnikova14@bk.ru>
2019-10-01 19:42:54 +0300

    Signed-off-by: Veto1234-ii <vetoshnikova14@bk.ru>

---
+++ кластеризация.py
@@ -0,0 +1,17 @@
+import random
+import numpy as np
+#N=100
+#k=3
+#n= float(N)/k# среднее, все точки делить на кол-во кластеров, среднее кол-во точек для одного кластера
+#X=[]
+#for i in range(k):
+#    c = (random.uniform(-1, 1), random.uniform(-1, 1))# uniform принимает от скольки до скольки(интервал) и размер. Это кортеж
+#    s=random.uniform(0.05,0.5) # значение
+#    x=[]
+#    while len(x)<n:
+#        a, b = np.array([np.random.normal(c[0], s), np.random.normal(c[1], s)])
+
+a, b = np.array([np.random.normal(c[0], s), np.random.normal(c[1], s)])   
+print(a)
+
+
--- файл тест.py
+++ файл тест.py
@@ -4,33 +4,32 @@ import numpy as np
 import matplotlib.pyplot as plt 
 import cv2
 
-lib = envi.open(r'C:\Users\User\Desktop\Спектральные изображения\f080709t01p00r15\f080709t01p00r15rdn_c_ort_igm.hdr', r'C:\Users\User\Desktop\Спектральные изображения\f080709t01p00r15\f080709t01p00r15rdn_c_ort_igm')
+lib = envi.open(r'C:\Users\IT\Documents\Image spectral\f090729t01p00r09\f090729t01p00r09rdn_b\f090729t01p00r09rdn_b_ort_igm.hdr', r'C:\Users\IT\Documents\Image spectral\f090729t01p00r09\f090729t01p00r09rdn_b\f090729t01p00r09rdn_b_ort_igm')
 
 print(lib.shape )
 print(lib[50,100])
 print(lib[50,100].shape)
-gt  = envi.open(r'C:\Users\User\Desktop\Спектральные изображения\f080709t01p00r15\f080709t01p00r15rdn_c_ort_igm.hdr', r'C:\Users\User\Desktop\Спектральные изображения\f080709t01p00r15\f080709t01p00r15rdn_c_ort_igm') . read_band ( 0 ) 
+gt  = envi.open(r'C:\Users\IT\Documents\Image spectral\f090729t01p00r09\f090729t01p00r09rdn_b\f090729t01p00r09rdn_b_ort_igm.hdr', r'C:\Users\IT\Documents\Image spectral\f090729t01p00r09\f090729t01p00r09rdn_b\f090729t01p00r09rdn_b_ort_igm') . read_band ( 1 ) 
 print(lib)
 print(type(gt))
 
-view  =  imshow (lib ,  ( 0 ,  1 ,  0 ), classes = gt )
+view  =  imshow (lib ,  ( 2 ,  2 ,  2 ), classes = gt )
 
 view.class_alpha = 0.5
 
 
-pc = principal_components(lib)
-v = imshow(pc.cov)
+#pc = principal_components(lib)
+#v = imshow(pc.cov)
 
 
 
 
-#( m ,  c )  =  kmeans ( lib ,  20 ,  30 ) 
+
+( m ,  c )  =  kmeans ( lib ,  2 ,  2 ) 
 
 
    
 
-
-#view_cube ( lib ,  band = [ 0 ,  1 ,  2 ])
 
 #save_rgb ( 'rgb.jpg' ,  lib ,  [ -1 ,  -1 ,  -1 ],colors = spy_colors)
 #data= envi.open(r'C:\Users\User\Desktop\Спектральные изображения\f080709t01p00r15\f080709t01p00r15rdn_c_ort_igm.hdr', r'C:\Users\User\Desktop\Спектральные изображения\f080709t01p00r15\f080709t01p00r15rdn_c_ort_igm').load()
